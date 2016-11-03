aa9be98fd31224ec38ae1779d0018237324b2687
Adam Westman <adam.westman@king.com>
2016-11-03 13:14:31 +0100

    Added additional building blocks

--- defkit/internal/lookup.lua
+++ defkit/internal/lookup.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local dynamic_variables = {}
+
 function M.add_object_by_type(object_type, game_object_id)
 	
 end
@@ -10,6 +12,25 @@ end
 
 function M.get_objects_by_type(object_type)
 	
+end
+
+function M.set_variable(hash, value)
+	assert(hash)
+	assert(value)
+	
+	dynamic_variables[hash] = value
+end
+
+function M.get_variable(hash, default_value)
+	assert(hash)
+	
+	if dynamic_variables[hash] then
+		return dynamic_variables[hash]
+	elseif not default_value then
+		error(string.format("Requested unknown variable %s", tostring(hash)))
+	end
+	
+	return default_value
 end
 
 return M
