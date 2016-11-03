858effea56a034c11f33aee55a1bf233b5cdeb1b
Adam Westman <adam.westman@king.com>
2016-11-03 11:09:55 +0100

    Fixed bugs and added example

---
+++ defkit/internal/lookup.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+function M.add_object_by_type(object_type, game_object_id)
+	
+end
+
+function M.remove_object(game_object_id)
+	
+end
+
+function M.get_objects_by_type(object_type)
+	
+end
+
+return M
